375c30a6d691fdf55aa413a12c581036d8953f1a
Christian Rey Villablanca <chriztiann.villablanca@gmail.com>
2022-02-02 17:30:06 +0800

    commit

--- admin/modules/student/list.php
+++ admin/modules/student/list.php
@@ -1,9 +1,4 @@
-<div class="wells">
-	<h3 align="left">List of Student</h3>
-	<form action="controller.php?action=delete" Method="POST">
-		<table id="example" class="table table-striped" cellspacing="0">
 
-<<<<<<< HEAD
 		<div class="wells">
 				<h3 align="left">List of Student</h3>
 			    <form action="controller.php?action=delete" Method="POST">  					
@@ -14,40 +9,22 @@
 				  		<th>No.</th>
 				  		<th width="10%" align="left"><input type="checkbox" name="chkall" id="chkall" onclick="return checkall('selector[]');"> ID#.</th>
 				  		<th>Fullname</th>
-				  		<!--	<th>1st Course</th>-->
-				  		<!--	<th>2nd Course</th>-->
+				  	
+				  	<!--	<th>2nd Course</th>-->
 				  	<!--	<th>3rd Course</th> -->
 				  		<th>Email Address</th>
-				  		<th>Options</th>
+						  <th>Details</th>
+				  		<th>Photo</th>
 				  	</tr>	
 				  </thead>
 				  <tbody>
 				  	<?php
 					 
 				  	  	$mydb->setQuery("SELECT  `IDNO` ,UPPER(CONCAT(  `LNAME` ,  ', ',  `FNAME` ,  ' ',  `MNAME` )) AS  'Name',
-=======
-			<thead>
-				<tr>
-					<th>No.</th>
-					<th width="10%" align="left"><input type="checkbox" name="chkall" id="chkall" onclick="return checkall('selector[]');"> ID#.</th>
-					<th>Fullname</th>
-					<th>1st Course</th>
-					<th>2nd Course</th>
-					<th>3rd Course</th>
-					<th>Email Address</th>
-					<th>Options</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php
-
-				$mydb->setQuery("SELECT  `IDNO` ,UPPER(CONCAT(  `LNAME` ,  ', ',  `FNAME` ,  ' ',  `MNAME` )) AS  'Name',
->>>>>>> f06061c57163a0e026c0fe0b5e74831e11bfd997
 				  						  `COURSE_ONE` ,`COURSE_TWO`, `COURSE_THREE` ,  `STATUS` ,  `EMAIL` , `attachment_file`
 				  						  FROM  `tblstudent`");
-				loadresult();
+				  	  	loadresult();
 
-<<<<<<< HEAD
 				  	
 				  		function loadresult(){
 							
@@ -75,69 +52,29 @@
 						//	 echo  '<img src="data:image/jpg;charset=utf8;base64,'.$imagepath.'"/> ';
 						//	 <img src="data:image/jpeg;base64,'.base64_encode($row['name'] ).'" height="200" width="200" class="img-thumnail" />  
 					  	//	echo '<td><a href = "index.php?view=view&studentId='.$student->IDNO.'" ><span class="glyphicon glyphicon-list-alt"> </span>  View</a></td>';
-=======
-
-				function loadresult()
-				{
-					global $mydb;
-					$cur = $mydb->loadResultList();
-					foreach ($cur as $student) {
-						echo '<tr>';
-						echo '<td width="5%" align="center"></td>';
-						echo '<td width="10%"><input type="checkbox" name="selector[]" id="selector[]" value="' . $student->IDNO . '"/>
-					  				<a href="index.php?view=edit&id=' . $student->IDNO . '">' . $student->IDNO . '</a></td>';
-						echo '<td  >' . $student->Name . '</td>';
-						echo '<td align="center">' . $student->COURSE_ONE . '</td>';
-						echo '<td  align="center">' . $student->COURSE_TWO . '</td>';
-						echo '<td  align="center">' . $student->COURSE_THREE . '</td>';
-						echo '<td>' . $student->EMAIL . '</td>';
-						$imagepath = base64_encode($student->attachment_file);
-
-						echo "<script>console.log('" . json_encode($imagepath) . "');</script>";
-
-
-						echo '<div>
-							  <td><a href = "index.php?view=view&studentId=' . $student->IDNO . '" ><span class="glyphicon glyphicon-list-alt"> </span>  Grades</a></td>
-							  <td><a href = "viewimage.php?pic=' . $imagepath . '" target="_blank" ><span class="glyphicon glyphicon-list-alt"> </span>  Image</a></td>
-							
-							  <form method="post" action="viewimage.php?pic=' . $imagepath . '" class="inline">
-							  <input type="hidden" name="extra_submit_param" value="extra_submit_value">
-							  <button type="submit" name="submit_param" value="submit_value" class="link-button">
-								This is a link that sends a POST request
-							  </button>
-							</form>
-							
-							
-							
-							  </div>';
-						echo  '<img src="data:attachment_file/jpg;charset=utf8;base64,' . $imagepath . '"/>';
-
-						//	echo '<td><a href = "index.php?view=view&studentId='.$student->IDNO.'" ><span class="glyphicon glyphicon-list-alt"> </span>  View</a></td>';
->>>>>>> f06061c57163a0e026c0fe0b5e74831e11bfd997
 						//	  echo '<td><a href = "index.php?view=view&studentId='.$student->IDNO.'" ><span class="glyphicon glyphicon-list-alt"> </span>  Image</a></td>';
-						echo '</tr>';
-					}
-				}
+					  		echo '</tr>';
+					  		}
 
-				?>
-
-
-			</tbody>
-
-		</table>
+				  		} 
+				  	
+				  	?>
 
 
+				  </tbody>
+				 
+				</table>
+			
+				
+						
+						<div class="btn-group">
+						  <a href="index.php?view=add" class="btn btn-default"><span class="glyphicon glyphicon-plus-sign"></span>  New</a>
+						   <button type="submit" class="btn btn-default" name="delete"><span class="glyphicon glyphicon-trash"></span> Delete Selected</button>
+						</div>
+				
 
-		<div class="btn-group">
-			<a href="index.php?view=add" class="btn btn-default"><span class="glyphicon glyphicon-plus-sign"></span> New</a>
-			<button type="submit" class="btn btn-default" name="delete"><span class="glyphicon glyphicon-trash"></span> Delete Selected</button>
-		</div>
+				
+				</form>
+	  	</div><!--End of well-->
 
-
-
-	</form>
-</div>
-<!--End of well-->
-
-</div>
-<!--End of container-->
+</div><!--End of container-->
